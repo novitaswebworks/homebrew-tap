3e48855f810262c6f5c89587b8a625c4e6e7b37d
lordbobby-dot <lmsapp119@gmail.com>
2026-08-22 13:18:04 +0530

    Update proc-manager to 1.2.0

--- Formula/proc-manager.rb
+++ Formula/proc-manager.rb
@@ -1,22 +1,22 @@
 class ProcManager < Formula
   desc "Modern TUI Process, Docker, and Service Manager"
   homepage "https://github.com/novitaswebworks/proc-manager"
-  version "1.0.1"
+  version "1.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/novitaswebworks/proc-manager/releases/download/v1.0.1/proc-manager-macos-arm64.tar.gz"
-      sha256 "03a54f22d3a87a938c48b7488a6fcd30e1c348d8e2faec1336236fb79cce56e4"
+      url "https://github.com/novitaswebworks/proc-manager/releases/download/1.2.0/proc-manager-macos-arm64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/novitaswebworks/proc-manager/releases/download/v1.0.1/proc-manager-macos-x86_64.tar.gz"
-      sha256 "0525bcd02679cac1f983b1613ff17b5ca49c2c66a16b708c5c332bcb41d3e4c7"
+      url "https://github.com/novitaswebworks/proc-manager/releases/download/1.2.0/proc-manager-macos-x86_64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/novitaswebworks/proc-manager/releases/download/v1.0.1/proc-manager-linux-x86_64.tar.gz"
-      sha256 "9511d05e2f6b3c2d8695063d1b9e87f879c9acd7c5e56738c9402f57adbb6765"
+      url "https://github.com/novitaswebworks/proc-manager/releases/download/1.2.0/proc-manager-linux-x86_64.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
